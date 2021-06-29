4bf3188b9212578821ccee97329e5050927b99b1
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-06-29 13:19:47 -0500

    Despliegue en Heroku

--- src/main/java/edu/escuelaing/arsw/app/BBAppStarter.java
+++ src/main/java/edu/escuelaing/arsw/app/BBAppStarter.java
@@ -17,6 +17,7 @@ public class BBAppStarter {
 		if (System.getenv("PORT") != null) {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
-		return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
+		return 8080; //returns default port if heroku-port isn't set
+					// (i.e. on localhost)
 	}
 }
