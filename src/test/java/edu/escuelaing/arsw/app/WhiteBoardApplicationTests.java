e3ed0a680b102bba85cd2c42ab80effd8895746b
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-06-29 12:10:00 -0500

    Modificacion HTML y se crea la el ENDPOINT

---
+++ src/test/java/edu/escuelaing/arsw/app/WhiteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
